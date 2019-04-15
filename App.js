fef58fc93205d69b8edb939054a837cf062694a7
epotts01 <goalietrooper@gmail.com>
2019-04-14 22:03:49 -0500

    practice

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Heat 'em up Speed 'em up!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#005eff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+});
