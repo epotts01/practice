41c65f2de5826ae0ddc8fb9a43c9ee4c80be8693
epotts01 <goalietrooper@gmail.com>
2019-05-22 22:27:00 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
 
 export default class App extends React.Component {
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Heat 'em up Speed 'em up!</Text>
-      </View>
+     
+      <ScrollView>
+        <View style={styles.container}>
+          <Text ></Text>
+          <Text></Text>
+          <Text></Text>
+          <Text>AP Chemistry</Text>
+          <Text>Heat 'em up Speed 'em up!</Text>
+          <Text></Text>
+          <Text></Text>
+          <Text></Text>
+          </View>
+          </ScrollView>
+      
+       
+      
     );
   }
+ /* render() {
+    return (
+      <View style={styles.container}>
+      <Text></Text>
+      </View>
+    );
+  } */
 }
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    
+    
     backgroundColor: '#005eff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 200,
     
   },
 });
